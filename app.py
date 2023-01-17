67a4f7f1d3aab2697f8c5129c9e4b85bd573ef96
Abdullah <122647095+Abdullah5111@users.noreply.github.com>
2023-01-18 02:10:11 +0500

    route /signup in app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
 	return render_template('homepage.html')
 
+@app.route('/signup')
+def signup():
+	return render_template('signup.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
